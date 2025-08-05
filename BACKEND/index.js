e9744a24ff81ae2a2e258969045caaf7d036ef67
Daiana Zabala <daiana.azabala@gmail.com>
2025-08-05 12:50:21 -0300

    Cambios de Backend

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,34 +1,29 @@
 import express from "express";
 import morgan from "morgan";
 import cors from 'cors';
-import { router as invitadosRouter } from "../BACKEND/routes.js";
-import eventosRouter from '../BACKEND/eventos.js';
-
-
+import { router as invitadosRouter } from "./backend/routes.js"; // Ajustá ruta si es necesario
+import eventosRouter from './backend/eventos.js';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-
 app.use(cors()); // Permite cualquier origen temporalmente
-
-// Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 
 // Rutas
 app.use('/api/invitados', invitadosRouter);
-app.use('/api/eventos', eventosRouter);  
+app.use('/api/eventos', eventosRouter);
 
 app.get('/', (req, res) => {
   res.send('Challenge Academia ForIt');
 });
 
-// Middleware de manejo de errores (syntax error JSON)
+// Middleware para manejar error de JSON inválido
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     console.error('JSON inválido:', err.message);
-    return res.status(400).json({ error: 'Debe ingresar nombre, DNI y status válidos.' });
+    return res.status(400).json({ error: 'JSON inválido' });
   }
   next(err);
 });
