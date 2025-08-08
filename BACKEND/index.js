6a4fdc504af960af0f91bc1cc020ffa00c3b87c6
DaiaZabala <daiana.azabala@gmail.com>
2025-08-07 21:11:07 -0300

    install cors

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,17 +1,37 @@
-import express from 'express';
-import cors from 'cors'; // Importa cors
+import express from "express";
+import morgan from "morgan";
+import cors from 'cors';
+import { router as invitadosRouter } from "../backend/routes.js";
+import eventosRouter from '../backend/eventos.js';
+
+
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(cors()); // Usa el middleware de CORS
+app.use(cors()); // Permite cualquier origen temporalmente
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Rutas
+app.use('/api/invitados', invitadosRouter);
+app.use('/api/eventos', eventosRouter);  
 
 app.get('/', (req, res) => {
   res.send('Challenge Academia ForIt');
 });
 
-// ... el resto de tu código
+// Middleware de manejo de errores (syntax error JSON)
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    console.error('JSON inválido:', err.message);
+    return res.status(400).json({ error: 'Debe ingresar nombre, DNI y status válidos.' });
+  }
+  next(err);
+});
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
