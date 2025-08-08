dd2f19672e20952c80fafe44692ce66871d06ab6
DaiaZabala <daiana.azabala@gmail.com>
2025-08-07 21:02:23 -0300

    install cors

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,37 +1,10 @@
-<<<<<<< HEAD
-import express from "express";
-import morgan from "morgan";
-import cors from 'cors';
-import { router as invitadosRouter } from "../backend/routes.js";
-import eventosRouter from '../backend/eventos.js';
-=======
 import express from 'express';
 import cors from 'cors'; // Importa cors
->>>>>>> 792083b6c6595918c6ebc40078f33bccc40de846
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-// Configura CORS para permitir solicitudes solo desde tu dominio de Vercel
-app.use(cors({
-  origin: 'https://eventdai.vercel.app'
-}));
-
-// Middlewares
-app.use(morgan('dev'));
-app.use(express.json());
-
-// Rutas
-app.use('/api/invitados', invitadosRouter);
-app.use('/api/eventos', eventosRouter); 
-
-app.get('/healthcheck', (req, res) => {
-  res.status(200).send('OK');
-});
-=======
 app.use(cors()); // Usa el middleware de CORS
->>>>>>> 792083b6c6595918c6ebc40078f33bccc40de846
 
 app.get('/', (req, res) => {
   res.send('Challenge Academia ForIt');
@@ -40,9 +13,5 @@ app.get('/', (req, res) => {
 // ... el resto de tu código
 
 app.listen(port, () => {
-<<<<<<< HEAD
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-=======
   console.log(`Servidor corriendo en el puerto ${port}`);
->>>>>>> 792083b6c6595918c6ebc40078f33bccc40de846
 });
