f1763b2cfe532c723369151e4b0403f8bc161166
DaiaZabala <daiana.azabala@gmail.com>
2025-08-12 22:07:15 -0300

    prueba

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite' 
-import vue from '@vitejs/plugin-vue' 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
-  // Aquí se agrega el proxy para las peticiones de la API
-  server: {
-    // Para resolver el problema de CORS, creamos un proxy
-    proxy: {
-      // Cualquier petición que comience con '/api' será redirigida
-      '/api': {
-        // El destino de la petición, que es la URL de tu API
-        target: 'https://academiaforit1.onrender.com', 
-        // Es importante para que el origen de la petición parezca venir del servidor de destino
-        changeOrigin: true,
-        // Re-escribe la ruta para eliminar el prefijo '/api' antes de enviarla
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  }
 })
